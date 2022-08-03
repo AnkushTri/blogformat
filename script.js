46b063f619825a11371b31986b90812030979a47
ankush <kumarankush13701@gmail.com>
2022-08-03 17:41:06 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("hello ankush");
+const para=document.querySelector(".article-body");
+const hiden=document.querySelector(".hide");
+// const btna= document.getElementsByClassName("btn");
+// btna.onclick=function(){
+//     console.log("jfk you are a");
+// }
+const buttom=document.querySelector("button");
+buttom.addEventListener("click",()=>{
+    hiden.style.display="block";
+    buttom.style.display = "none";
+    // para.innerHTML+="l lore,maf afkjafdsjfd jalfjadskf fja sdjealwj"
+});
+// buttom.style.display = "none";
